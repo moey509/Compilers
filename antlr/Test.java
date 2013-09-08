b6552c06a09dc194ab66d28f294975a49f8aeaec
James Feher <jkf49@cornell.edu>
2013-09-07 23:03:18 -0400

    Changed Test

--- antlr/Test.java
+++ antlr/Test.java
@@ -18,7 +18,7 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		// Create HashMap. ID number -> token name
 		HashMap<Integer, String> map = new HashMap<Integer, String>();
-		Scanner sc = new Scanner(new BufferedReader(new FileReader("CubexLexer.tokens")));
+		Scanner sc = new Scanner(new BufferedReader(new FileReader("antlr/CubexLexer.tokens")));
 		while(sc.hasNext()){
 			String s = sc.nextLine();
 			if(!s.contains("'")){
@@ -29,28 +29,40 @@ public class Test {
 		
 		//Lexing starts here
 		CubexLexer lex = new CubexLexer(new ANTLRFileStream(args[0]));
-		if(lex.hasErred){ //Doesn't do anything right now
-			System.out.println("error");
-			return;
-		}
-		
+//		if(lex.hasErred){ //Doesn't do anything right now
+//			System.out.println("error");
+//			return;
+//		}
+		boolean firstTime = true;
 		//Change tokens based on PA1 stuff
 		for(Token t : lex.getAllTokens()){
 			//Need conditions for variable names and type names
-			if (map.get(t.getType()).equalsIgnoreCase("Integer")) {
-				System.out.println(0);
-			}
-			else if (map.get(t.getType()).equalsIgnoreCase("false") || map.get(t.getType()).equalsIgnoreCase("true")){
-				System.out.println("true");
-			}
-			else if(map.get(t.getType()).equalsIgnoreCase("String")){
-				System.out.println("\"\"");
-			}
-			else if(map.get(t.getType()).equalsIgnoreCase("Comment") || map.get(t.getType()).equalsIgnoreCase("NestedComment") || map.get(t.getType()).equalsIgnoreCase("WS")){
-				System.out.println("\"\"");
+			if(!firstTime){
+				firstTime = false;
 			}
 			else{
-				System.out.print(t.getText()+ " ");
+				System.out.print(" ");
+			}
+			if (map.get(t.getType()).equalsIgnoreCase("Integer")) {
+				System.out.print(0);
+			}
+			else if (map.get(t.getType()).equalsIgnoreCase("false") || map.get(t.getType()).equalsIgnoreCase("true")){
+				System.out.print("true");
+			}
+			else if(map.get(t.getType()).equalsIgnoreCase("String")){
+				System.out.print("\"\"");
+			}
+			else if(map.get(t.getType()).equalsIgnoreCase("Comment") || map.get(t.getType()).equalsIgnoreCase("NestedComment") || map.get(t.getType()).equalsIgnoreCase("WS")){
+				System.out.print("");
+			}
+			else if(map.get(t.getType()).equalsIgnoreCase("VARFUNID")){
+				System.out.print("name");
+			}
+			else if(map.get(t.getType()).equalsIgnoreCase("TYPEID")){
+				System.out.print("Name");
+			}
+			else{
+				System.out.print(t.getText());
 			}
 		}
 	}
