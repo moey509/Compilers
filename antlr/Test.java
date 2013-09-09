4a4f03f5ef62a7ac1bef3c37da43a6d65a974ea1
James Feher <jkf49@cornell.edu>
2013-09-08 22:14:56 -0400

    Added error handling

--- antlr/Test.java
+++ antlr/Test.java
@@ -1,16 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.BitSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.Nullable;
 
 public class Test {
 
+	static boolean lexedWithError = false;
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -33,9 +44,19 @@ public class Test {
 //			System.out.println("error");
 //			return;
 //		}
+		lex.removeErrorListeners();
+		
+		LexerErrorListener listener = new LexerErrorListener();
+		lex.addErrorListener(listener);
+		
 		boolean firstTime = true;
 		//Change tokens based on PA1 stuff
-		for(Token t : lex.getAllTokens()){
+		List<? extends Token> list = lex.getAllTokens();
+		if(lexedWithError){
+			System.out.println("error");
+			return;
+		}
+		for(Token t : list){
 			//Need conditions for variable names and type names
 			if(firstTime){
 				firstTime = false;
@@ -64,6 +85,45 @@ public class Test {
 			else{
 				System.out.print(t.getText());
 			}
+		}
+	}
+	
+	static class LexerErrorListener implements ANTLRErrorListener {
+
+		@Override
+		public void reportAmbiguity(@NotNull Parser arg0, @NotNull DFA arg1,
+				int arg2, int arg3, boolean arg4, @NotNull BitSet arg5,
+				@NotNull ATNConfigSet arg6) {
+			// TODO Auto-generated method stub
+			return;
+		}
+
+		@Override
+		public void reportAttemptingFullContext(@NotNull Parser arg0,
+				@NotNull DFA arg1, int arg2, int arg3, @Nullable BitSet arg4,
+				@NotNull ATNConfigSet arg5) {
+			// TODO Auto-generated method stub
+			return;
+		}
+
+		@Override
+		public void reportContextSensitivity(@NotNull Parser arg0,
+				@NotNull DFA arg1, int arg2, int arg3, int arg4,
+				@NotNull ATNConfigSet arg5) {
+			// TODO Auto-generated method stub
+			return;
+		}
+		
+		private class lexException extends Exception{
+			public lexException(){ super(); }
+			
+		}
+
+		@Override
+		public void syntaxError(Recognizer<?, ?> arg0, @Nullable Object arg1,
+				int arg2, int arg3, String arg4, @Nullable RecognitionException arg5) {
+			// TODO Auto-generated method stub
+			lexedWithError = true;
 		}
 	}
 
