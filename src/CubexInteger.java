68bed6b5366f3bee13336d64f95dd5f3ae26945e
moey509 <mso32@cornell.edu>
2013-09-22 00:57:53 -0400

    Changed some shit

--- src/CubexInteger.java
+++ src/CubexInteger.java
@@ -1,6 +1,6 @@
 public final class CubexInteger extends CubexExpression {
 	private int mValue;
-	public CubexInteger(int value) {mvalue = value;}
+	public CubexInteger(int value) {mValue = value;}
 	protected CubexType calculateType(CubexContext context) {
 		return CubexType.getInt();
 	}
--- src/NoSuchTypeException.java
+++ src/NoSuchTypeException.java
@@ -1,1 +1,11 @@
-public class NoSuchTypeException extends Exception {}
+public class NoSuchTypeException extends Exception {
+	
+	public NoSuchTypeException() {
+		super();
+	}
+	
+	public NoSuchTypeException(String s) {
+		super(s);
+	}
+	
+}
