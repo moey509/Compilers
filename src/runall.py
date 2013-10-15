5ec7dacc52f0a1473c764714a9ff9b7e1a733cc6
unknown <tn229@cornell.edu>
2013-10-15 00:03:32 -0400

    modified test script so that it actaully type chekcs

--- src/runall.py
+++ src/runall.py
@@ -14,10 +14,10 @@ print "[INFO] Finished Compiling..."
 
 print "[INFO] Started Running files.."
 
-for fname in sorted(glob.glob("../parser_tests/*.in"), key=getnum):
+for fname in sorted(glob.glob("../semantics_tests/*.in"), key=getnum):
     num = getnum(fname)
-    print "[INFO] running java parser.CubexParserMain %s" % fname
-    myout = subprocess.check_output("java parser.CubexParserMain %s" % fname, shell=True)
+    print "[INFO] running java typeChecker.TypeCheckerMain %s" % fname
+    myout = subprocess.check_output("java typeChecker.TypeCheckerMain  %s" % fname, shell=True)
     if not isinstance(myout, str): myout = str(myout, encoding='utf-8')
     expectedout = open(fname[:-len("in")] + "out").read()
     # Strip trailing newline
