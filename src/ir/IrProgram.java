07e1ddaf237850a467d062ce81f0f023403a2e9b
Ansha Yu <anshajyu@gmail.com>
2013-10-26 21:15:17 -0400

    oops forgot stuff in IrProgram

--- src/ir/IrProgram.java
+++ src/ir/IrProgram.java
@@ -1,5 +1,11 @@
 package ir;
 
-public class IrProgram {
+import java.util.ArrayList;
 
+public class IrProgram {
+	ArrayList<IrProgramElem> components;
+	
+	public IrProgram(ArrayList<IrProgramElem> c) {
+		components = c;
+	}
 }
