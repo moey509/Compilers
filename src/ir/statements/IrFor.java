d1a01c1dd77a4900b7021e15d533d8210ea1f51a
James Feher <jkf49@cornell.edu>
2013-11-02 17:40:51 -0400

    part of irfor implemented

--- src/ir/statements/IrFor.java
+++ src/ir/statements/IrFor.java
@@ -3,6 +3,8 @@ package ir.statements;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import ir.CGenerationContext;
 import ir.expressions.IrExpression;
 
@@ -26,9 +28,14 @@ public class IrFor implements IrStatement {
 	@Override
 	public ArrayList<String> toC(CGenerationContext context) {
 		// TODO TOSHI DO SOMETHING HERE
-		return null;
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("for(" + condition.toC(context) + "; " + "){");
+		for(IrStatement s : statements){
+			arr.addAll(s.toC(context));
+		}
+		arr.add("}");
+		return arr;
 	}
-
 }
 
 
