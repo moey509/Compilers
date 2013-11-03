3036fe8164892d2574dc3434b3f7721d845264b6
Toshi Noguchi <tn229@cornell.edu>
2013-11-03 01:37:46 -0500

    IROnwards complete

--- src/ir/expressions/IrOnwards.java
+++ src/ir/expressions/IrOnwards.java
@@ -1,5 +1,6 @@
 package ir.expressions;
 
+import ir.CGenerationContext;
 import ir.IrMiscFunctions;
 
 public class IrOnwards extends IrUnaryExpression {
@@ -17,16 +18,25 @@ public class IrOnwards extends IrUnaryExpression {
 		return type;
 	}
 	
-	public String toC() {
+	public String toC(CGenerationContext context) {
 		// int case
+		// new_git_int(-1, 5, 0);
 		if (expression.getType().equals("integer")) {
-			
+			String in;
+			if (include)
+				in = "+ 1";
+			else
+				in = "+ 0";
+			return "new_git_int(1, (" + expression.toC(context) + in + "), 0);";
 		}
 		// boolean case
 		else {
-			
+			String in;
+			if (include)
+				in = "+ 1";
+			else
+				in = "+ 0";
+			return "new_git_int(0, (" + expression.toC(context) + in + "), 1);";
 		}
-		
-		return null;
 	}
 }
