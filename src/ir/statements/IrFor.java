d50a584d138ecfd7c46ede75051bc37f7eb39bd9
James Feher <jkf49@cornell.edu>
2013-12-17 19:25:21 -0500

    Re-added garbage collection

--- src/ir/statements/IrFor.java
+++ src/ir/statements/IrFor.java
@@ -77,9 +77,9 @@ public class IrFor extends IrStatement {
 		// there is no CubexTypeGrammar for the expression, and the arguments don't have IrTypes
 		// can discuss later?
 		String iterDeclaration = iterable + " = iterable_append((" + list.toC(context) + "), NULL);";
-//		String inc1Declaration = "ref_increment((General_t)" + iterable + ");";
+		String inc1Declaration = "ref_increment((General_t)" + iterable + ");";
 		String itDeclaration = iterator + " = new_iterator((" + iterable + "));";
-//		String inc2Declaration = "ref_increment((General_t)" + iterator + ");";
+		String inc2Declaration = "ref_increment((General_t)" + iterator + ");";
 
 		//add iterable to list of stuff declared at the top of the function
 		context.varDecl.put(iterable, "git_t");
@@ -90,33 +90,33 @@ public class IrFor extends IrStatement {
 //		context.fcnVarDecl.put(iterator, "iterator_t");
 		String itCondition = "while(hasNext(" + iterator + ")) {";
 		String tempVar = "void* " + var + " = getNext(" + iterator + ");";
-//		String tempVarInc = "ref_increment((General_t)" + var + ");";
+		String tempVarInc = "ref_increment((General_t)" + var + ");";
 
 
 		output.add(iterDeclaration);
-//		output.add(inc1Declaration);
+		output.add(inc1Declaration);
 		output.add(itDeclaration);
-//		output.add(inc2Declaration);
+		output.add(inc2Declaration);
 		
 		//Should be replaced by Ansha's code
 		if(context.lva){
 			if(hasFreeBefore){
 				for(String s : freeBefore){
 //					System.out.println("Free Before For: " + s);
-//					output.add("ref_decrement((General_t)" + s + ");");
+					output.add("ref_decrement((General_t)" + s + ");");
 					output.add(s + " = NULL;");
 				}
 			}
 			for(String s : inMinusOut()){
 //				System.out.println("Free inMinusOut For: " + s);
-//				output.add("ref_decrement((General_t)" + s + ");");
+				output.add("ref_decrement((General_t)" + s + ");");
 				output.add(s + " = NULL;");
 			}
 		}
 		else{
 			for(IrBind b : this.temporaryBinds){
 				String s = b.tuple.variableName;
-//				output.add("ref_decrement((General_t)" + s + ");");
+				output.add("ref_decrement((General_t)" + s + ");");
 			}
 		}
 		output.add(itCondition);
@@ -137,7 +137,7 @@ public class IrFor extends IrStatement {
 
 		output.add(tempVar);
 		if(!context.lva || freeAfter.contains(var)){
-//			output.add(tempVarInc);
+			output.add(tempVarInc);
 		}
 
 		/*** vvv THE FOLLOWING IS A CODE BLOCK ***/
@@ -157,7 +157,7 @@ public class IrFor extends IrStatement {
 		/*** ^^^^ END CODE BLOCK ***/
 		
 		if(context.lva){
-//			output.add("ref_decrement((General_t)" + var + ");");
+			output.add("ref_decrement((General_t)" + var + ");");
 			output.add(var + " = NULL;");
 		}
 		String endLoop = "}";
@@ -165,20 +165,20 @@ public class IrFor extends IrStatement {
 		if(context.lva){
 			for(String s : freeAfter){
 				if(!s.equals(var)){
-//					output.add("ref_decrement((General_t)" + s + ");");
+					output.add("ref_decrement((General_t)" + s + ");");
 					output.add(s + " = NULL;");
 				}
 			}
 		}
 		
 		///Shouldn't be replaced by Ansha's code?...what if there are two for loops in a row with the same iterable? who cares?
-//		String dec1Declaration = "ref_decrement((General_t)" + iterable + ");";
-//		String dec2Declaration = "ref_decrement((General_t)" + iterator + ");";
+		String dec1Declaration = "ref_decrement((General_t)" + iterable + ");";
+		String dec2Declaration = "ref_decrement((General_t)" + iterator + ");";
 		String null1Declaration = iterable + " = NULL;";
 		String null2Declaration = iterator + " = NULL;";
 		
-//		output.add(dec1Declaration);
-//		output.add(dec2Declaration);
+		output.add(dec1Declaration);
+		output.add(dec2Declaration);
 		output.add(null1Declaration);
 		output.add(null2Declaration);
 
@@ -186,7 +186,7 @@ public class IrFor extends IrStatement {
 		//Should be replaced by Ansha's code methinks
 		if(!context.lva){
 			for (String s : freeContext) {
-//				output.add("ref_decrement((General_t)" + s + ");");
+				output.add("ref_decrement((General_t)" + s + ");");
 			}
 		}
 		
